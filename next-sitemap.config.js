68e5c9cc1ffccaa0493c6a56d57366c57019e49a
Kirill Spinei <kirill2022vip17@gmail.com>
2026-05-10 23:18:27 +1200

    updates

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    siteUrl: 'https://antitrud.vercel.app/',
+    generateRobotsTxt: true, // заодно создаст правильный robots.txt
+    exclude: ['/icon.png', '/apple-icon.png'], // исключить лишнее
+}
